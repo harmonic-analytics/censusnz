b74c948ebd849c39c12ef2fd0e4f84e7a33b885b
Alex Farmiloe <alex.f@medialab.local>
2020-11-27 17:14:50 +1300

    function skeleton for censusnz.R

---
+++ R/censusnz.R
@@ -0,0 +1,19 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_data() <- function() {
+
+}
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_variables() <- function() {
+
+}
