e99a80a4855a81c83bf737e92ddf4686f99aea83
Alex Farmiloe <alex@harmonic.co.nz>
2020-12-21 16:52:27 +1300

    add regions argument and tests to plot_data

--- tests/testthat/test-plot_data.R
+++ tests/testthat/test-plot_data.R
@@ -1,5 +1,9 @@
 test_that("plot_data runs without error", {
   expect_silent(plot_data(geography = "RC",
-                              variables = "smoking_status",
-                              n = 2))
+                          variables = "smoking_status",
+                          n = 2))
+  expect_silent(plot_data(geography = "RC",
+                          variables = "smoking_status",
+                          regions = "Auckland Region",
+                          n = 2))
 })
